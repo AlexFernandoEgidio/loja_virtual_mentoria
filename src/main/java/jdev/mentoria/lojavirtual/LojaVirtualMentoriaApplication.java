c23094cf3f3eb0237da074e74b5f68ddbae56911
alex_ <alex_@DESKTOP-MSGE99P>
2021-11-09 19:22:49 -0300

    commit inicial projeto mentoria Java

---
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

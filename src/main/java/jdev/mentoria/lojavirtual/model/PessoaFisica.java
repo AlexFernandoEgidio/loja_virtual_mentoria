ee41afd0780ca19ff1886ea609584b2451da6f66
alex_ <alex_@DESKTOP-MSGE99P>
2021-11-16 20:43:34 -0300

    rrrrr

--- src/main/java/jdev/mentoria/lojavirtual/model/PessoaFisica.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/PessoaFisica.java
@@ -3,12 +3,8 @@ package jdev.mentoria.lojavirtual.model;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -16,27 +12,14 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "pessoa_fisica")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) /* Classe concreta */
-@SequenceGenerator(name = "seq_pessoa_fisica", sequenceName = "seq_pessoa_fisica", allocationSize = 1, initialValue = 1)
 public class PessoaFisica extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa_fisica")
-	private Long id;
 
 	private String cpf;
 
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getCpf() {
 		return cpf;
@@ -52,31 +35,6 @@ public class PessoaFisica extends Pessoa {
 
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PessoaFisica other = (PessoaFisica) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
--- src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -1,25 +1,16 @@
 package jdev.mentoria.lojavirtual.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "pessoa_juridica")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) /* Classe concreta */
-@SequenceGenerator(name = "seq_pessoa_juridica", sequenceName = "seq_pessoa_juridica", allocationSize = 1, initialValue = 1)
 public class PessoaJuridica extends Pessoa {
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_pessoa_juridica")
-	private Long id;
 
 	private String cnpj;
 
@@ -32,14 +23,6 @@ public class PessoaJuridica extends Pessoa {
 	private String razao_social;
 
 	private String categoria;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getCnpj() {
 		return cnpj;
@@ -87,31 +70,6 @@ public class PessoaJuridica extends Pessoa {
 
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PessoaJuridica other = (PessoaJuridica) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }

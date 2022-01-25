2c70ed47bec73ee301e2d9462410911ebb762b0b
alex_ <alex_@DESKTOP-MSGE99P>
2022-01-25 20:12:55 -0300

    Mentoria 25-01-2022

--- src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
@@ -36,8 +36,19 @@ public abstract class Pessoa implements Serializable {
 	@Column(nullable = false)
 	private String telefone;
 	
+	@Column
+	private String tipoPessoa; 
+	
 	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Endereco> enderecos = new ArrayList<Endereco>();
+	
+	public void setTipoPessoa(String tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
+	}
+	
+	public String getTipoPessoa() {
+		return tipoPessoa;
+	}
 	
 	
 	public void setEnderecos(List<Endereco> enderecos) {
--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -22,6 +23,7 @@ import jdev.mentoria.lojavirtual.model.Acesso;
 import jdev.mentoria.lojavirtual.repository.AcessoRepository;
 import junit.framework.TestCase;
 
+@Profile("test")
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
 public class LojaVirtualMentoriaApplicationTests extends TestCase {
 	

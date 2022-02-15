ba4cb42e5b77f27b96323ef51378b99ff07b0132
alex_ <alex_@DESKTOP-MSGE99P.mshome.net>
2022-02-15 20:01:43 -0300

    mentoria 15-02-2022

---
+++ src/main/java/jdev/mentoria/lojavirtual/ControleExcecoes.java
@@ -0,0 +1,82 @@
+package jdev.mentoria.lojavirtual;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jdev.mentoria.lojavirtual.model.dto.ObjetoErroDTO;
+
+@RestControllerAdvice
+@ControllerAdvice
+public class ControleExcecoes extends ResponseEntityExceptionHandler {
+	
+	/*Captura execeçoes do projeto*/
+	@ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		
+		ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+		
+		String msg = "";
+		
+		if (ex instanceof MethodArgumentNotValidException) {
+			
+			List<ObjectError> list = ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+			
+			for (ObjectError objectError : list) {
+				msg += objectError.getDefaultMessage() + "\n";
+			}
+		}else {
+			msg = ex.getMessage();
+		}
+		
+		objetoErroDTO.setError(msg);
+		objetoErroDTO.setCode(status.value() + " ==> " + status.getReasonPhrase()); 
+		
+		return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	/*Captura erro na parte de banco*/
+	@ExceptionHandler({DataIntegrityViolationException.class, 
+			ConstraintViolationException.class, SQLException.class})
+	protected ResponseEntity<Object> handleExceptionDataIntegry(Exception ex){
+		
+		ObjetoErroDTO objetoErroDTO = new ObjetoErroDTO();
+		
+		String msg = "";
+		
+		if (ex instanceof DataIntegrityViolationException) {
+			msg = "Erro de integridade no banco: " +  ((DataIntegrityViolationException) ex).getCause().getCause().getMessage();
+		}else
+		if (ex instanceof ConstraintViolationException) {
+			msg = "Erro de chave estrangeira: " + ((ConstraintViolationException) ex).getCause().getCause().getMessage();
+		}else
+		if (ex instanceof SQLException) {
+			msg = "Erro de SQL do Banco: " + ((SQLException) ex).getCause().getCause().getMessage();
+		}else {
+			msg = ex.getMessage();
+		}
+		
+		objetoErroDTO.setError(msg);
+		objetoErroDTO.setCode(HttpStatus.INTERNAL_SERVER_ERROR.toString()); 
+		
+		return new ResponseEntity<Object>(objetoErroDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+
+}
---
+++ src/main/java/jdev/mentoria/lojavirtual/model/dto/ObjetoErroDTO.java
@@ -0,0 +1,29 @@
+package jdev.mentoria.lojavirtual.model.dto;
+
+import java.io.Serializable;
+
+public class ObjetoErroDTO implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private String error;
+	private String code;
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	
+	
+
+}

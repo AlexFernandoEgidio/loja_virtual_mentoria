0e39262034cc3fcae2feee398983d59486fe1b13
alex_ <alex_@DESKTOP-MSGE99P.mshome.net>
2022-02-15 18:17:42 -0300

    dd

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -17,11 +16,6 @@ public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
 		
-
-
-
-		System.out.println(new BCryptPasswordEncoder().encode("123"));//
-
 		
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
 	}

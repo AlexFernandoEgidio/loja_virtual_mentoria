4904bc5b55200252aa51fc3735e183526ec1ff99
alex_ <alex_@DESKTOP-MSGE99P.mshome.net>
2022-04-19 18:43:52 -0300

    dd

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -26,7 +26,7 @@ public class LojaVirtualMentoriaApplication implements AsyncConfigurer {
 
 	public static void main(String[] args) {
 		
-		System.out.println(new BCryptPasswordEncoder().encode("123"));
+		//System.out.println(new BCryptPasswordEncoder().encode("123"));
 		
 		
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);

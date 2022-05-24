553232f1446038485b0c948ee5b23c3e239f0402
alex_ <alex_@DESKTOP-MSGE99P>
2022-05-24 20:14:03 -0300

    ddddd

--- src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
@@ -18,6 +18,7 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -38,6 +39,7 @@ public class NotaFiscalCompra implements Serializable {
 	@Column(nullable = false)
 	private String numeroNota;
 	
+	@NotEmpty(message = "Informe a serie da nota")
 	@NotNull(message = "Informe a série da nota")
 	@Column(nullable = false)
 	private String serieNota;

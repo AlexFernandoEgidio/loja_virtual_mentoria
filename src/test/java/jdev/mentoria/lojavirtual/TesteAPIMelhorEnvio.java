fb4623df446da3c17051565accee7c90a7ffcfa8
afgale <alex.fernando.egidio@gmail.com>
2022-10-11 17:18:28 -0300

    jjd

---
+++ src/test/java/jdev/mentoria/lojavirtual/TesteAPIMelhorEnvio.java
@@ -0,0 +1,33 @@
+package jdev.mentoria.lojavirtual;
+
+
+import jdev.mentoria.lojavirtual.enums.ApiTokenIntegracao;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class TesteAPIMelhorEnvio {
+	
+	public static void main(String[] args) throws Exception {
+		
+		       OkHttpClient client = new OkHttpClient().newBuilder() .build(); /*Instancia o objto de requisicao*/
+				MediaType mediaType = MediaType.parse("application/json"); /*Tipo do dados em JSON*/
+				RequestBody body = RequestBody.create(mediaType, "{ \"from\": { \"postal_code\": \"96020360\" }, \"to\": { \"postal_code\": \"01018020\" }, \"products\": [ { \"id\": \"x\", \"width\": 11, \"height\": 17, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 10.1, \"quantity\": 1 }, { \"id\": \"y\", \"width\": 16, \"height\": 25, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 55.05, \"quantity\": 2 }, { \"id\": \"z\", \"width\": 22, \"height\": 30, \"length\": 11, \"weight\": 1, \"insurance_value\": 30, \"quantity\": 1 } ] }");
+				Request request = new Request.Builder()
+				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX +"api/v2/me/shipment/calculate")
+				  .method("POST", body)
+				  .addHeader("Accept", "application/json")
+				  .addHeader("Content-Type", "application/json")
+				  .addHeader("Authorization", "Bearer " + ApiTokenIntegracao.TOKEN_MELHOR_ENVIO_SAND_BOX)
+				  .addHeader("User-Agent", "suporte@jdevtreinamento.com.br")
+				  .build();
+				
+				Response response = client.newCall(request).execute();
+				System.out.println(request.body().toString());
+		
+	}
+
+
+}

5464670f201106821cb1fb24672ddd07db605c9d
afgale <alex.fernando.egidio@gmail.com>
2022-10-16 18:12:32 -0300

    rr

--- src/test/java/jdev/mentoria/lojavirtual/TesteAPIMelhorEnvio.java
+++ src/test/java/jdev/mentoria/lojavirtual/TesteAPIMelhorEnvio.java
@@ -1,6 +1,5 @@
 package jdev.mentoria.lojavirtual;
 
-
 import jdev.mentoria.lojavirtual.enums.ApiTokenIntegracao;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -12,8 +11,10 @@ public class TesteAPIMelhorEnvio {
 	
 	public static void main(String[] args) throws Exception {
 		
-		       OkHttpClient client = new OkHttpClient().newBuilder() .build(); /*Instancia o objto de requisicao*/
-				MediaType mediaType = MediaType.parse("application/json"); /*Tipo do dados em JSON*/
+	
+		
+		     OkHttpClient client = new OkHttpClient().newBuilder() .build();
+				MediaType mediaType = MediaType.parse("application/json");
 				RequestBody body = RequestBody.create(mediaType, "{ \"from\": { \"postal_code\": \"96020360\" }, \"to\": { \"postal_code\": \"01018020\" }, \"products\": [ { \"id\": \"x\", \"width\": 11, \"height\": 17, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 10.1, \"quantity\": 1 }, { \"id\": \"y\", \"width\": 16, \"height\": 25, \"length\": 11, \"weight\": 0.3, \"insurance_value\": 55.05, \"quantity\": 2 }, { \"id\": \"z\", \"width\": 22, \"height\": 30, \"length\": 11, \"weight\": 1, \"insurance_value\": 30, \"quantity\": 1 } ] }");
 				Request request = new Request.Builder()
 				  .url(ApiTokenIntegracao.URL_MELHOR_ENVIO_SAND_BOX +"api/v2/me/shipment/calculate")
@@ -25,7 +26,7 @@ public class TesteAPIMelhorEnvio {
 				  .build();
 				
 				Response response = client.newCall(request).execute();
-				System.out.println(request.body().toString());
+				System.out.println(response.body().string()); 
 		
 	}
 

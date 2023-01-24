75689435a97f00516684c205e68c42ea62720a04
afgale <alex.fernando.egidio@gmail.com>
2023-01-24 14:32:52 -0300

    ddd

--- src/main/java/jdev/mentoria/lojavirtual/controller/RecebePagamentoWebHookApiJuno.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/RecebePagamentoWebHookApiJuno.java
@@ -44,6 +44,7 @@ public class RecebePagamentoWebHookApiJuno implements Serializable {
 			 if (boletoJuno != null && !boletoJuno.isQuitado() && boletoPago) {
 				 
 				 boletoJunoRepository.quitarBoletoById(boletoJuno.getId());
+				 System.out.println("Boleto: " + boletoJuno.getCode() + " foi quitado ");
 				 
 			 }
 		}

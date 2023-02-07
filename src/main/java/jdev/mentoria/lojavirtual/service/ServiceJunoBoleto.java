165fb12ca76dde719046aef7374cef2354f56962
afgale <alex.fernando.egidio@gmail.com>
2023-02-07 16:20:59 -0300

    yy

--- src/main/java/jdev/mentoria/lojavirtual/service/ServiceJunoBoleto.java
+++ src/main/java/jdev/mentoria/lojavirtual/service/ServiceJunoBoleto.java
@@ -261,5 +261,47 @@ public class ServiceJunoBoleto implements Serializable {
 		
 	}
 	
+	public String listaWebHook() throws Exception {
+		
+	    AccessTokenJunoAPI accessTokenJunoAPI = this.obterTokenApiJuno();
+		
+		Client client = new HostIgnoringCliente("https://api.juno.com.br/").hostIgnoringCliente();
+		WebResource webResource = client.resource("https://api.juno.com.br/notifications/webhooks");
+		
+		ClientResponse clientResponse = webResource
+				.accept("application/json;charset=UTF-8")
+				.header("Content-Type", "application/json")
+				.header("X-API-Version", 2)
+				.header("X-Resource-Token", ApiTokenIntegracao.TOKEN_PRIVATE_JUNO)
+				.header("Authorization", "Bearer " + accessTokenJunoAPI.getAccess_token())
+				.get(ClientResponse.class);
+		
+		 String resposta = clientResponse.getEntity(String.class);
+		 
+		return resposta;
+		
+	}
+	
+	
+	
+	public void deleteWebHook(String idWebHook) throws Exception {
+		
+	    AccessTokenJunoAPI accessTokenJunoAPI = this.obterTokenApiJuno();
+		
+		Client client = new HostIgnoringCliente("https://api.juno.com.br/").hostIgnoringCliente();
+		WebResource webResource = client.resource("https://api.juno.com.br/notifications/webhooks/" + idWebHook);
+		
+		webResource
+				.accept("application/json;charset=UTF-8")
+				.header("Content-Type", "application/json")
+				.header("X-API-Version", 2)
+				.header("X-Resource-Token", ApiTokenIntegracao.TOKEN_PRIVATE_JUNO)
+				.header("Authorization", "Bearer " + accessTokenJunoAPI.getAccess_token())
+				.delete();
+		
+		
+	}
+
+	
 
 }
--- src/test/java/jdev/mentoria/lojavirtual/TesteJunoBoleto.java
+++ src/test/java/jdev/mentoria/lojavirtual/TesteJunoBoleto.java
@@ -16,13 +16,32 @@ public class TesteJunoBoleto extends TestCase {
 	@Autowired
 	private ServiceJunoBoleto serviceJunoBoleto;
 	
+	
+	
+	@Test
+	public void deleteWebHook() throws Exception {
+		
+		serviceJunoBoleto.deleteWebHook("wbh_E71095B5BF65E8D2DB018EE8A89BACB8");
+		
+	}
+	
+
+	
+	@Test
+	public void listaWebHook() throws Exception {
+		
+		String retorno = serviceJunoBoleto.listaWebHook();
+		
+		System.out.println(retorno);
+	}
+	
 	@Test
 	public void testeCriarWebHook() throws Exception {
 		
 		CriarWebHook criarWebHook = new CriarWebHook();
-		criarWebHook.setUrl("https://lojavirtualmentoria-env.eba-bijtuvkg.sa-east-1.elasticbeanstalk.com/loja_virtual_mentoria/requisicaojunoboleto/notificacaoapiv2");
-		criarWebHook.getEventTypes().add("PAYMENT_NOTIFICATION");
+		criarWebHook.setUrl("https://api2.lojavirtualjdev.com.br/loja_virtual_mentoria/requisicaojunoboleto/notificacaoapiv2");
 		criarWebHook.getEventTypes().add("BILL_PAYMENT_STATUS_CHANGED");
+		criarWebHook.getEventTypes().add("PAYMENT_NOTIFICATION");
 		
 		String retorno = serviceJunoBoleto.criarWebHook(criarWebHook);
 		
